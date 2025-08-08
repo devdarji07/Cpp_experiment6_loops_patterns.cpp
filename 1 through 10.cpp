39150f1432004654ae0651ebb518e8a811bd56e2
Dev <devkdarji@gmail.com>
2025-08-08 21:22:38 +0530

    Create 1 through 10.cpp

---
+++ 1 through 10.cpp
@@ -0,0 +1,13 @@
+// Dev Darji
+// PRN - 24070123033
+// ENTC A2
+// EXPERIMENT 06
+
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i=1; i<=10; i++) {
+        cout<< i <<endl;
+    }
+    return 0;
+}
